c6a7f7f38cbbc701ef3e43d8db40689a1996b892
Daniel Guberina <guberinadaniel@gmail.com>
2017-06-14 19:09:23 +0200

    Napravljen task_1 sa svim dir.

---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/font-awesome.css" type="text/css">
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:700" rel="stylesheet">
+    <title>Zadatak 1 | Taask 1</title>
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="naslov">
+            <h1>This is the Title</h1>
+            <h2>This is an awesome headline</h2>
+        </div>
+        <hr>
+        <div class="autor">
+            <p>WRITEN BY<br>
+                <strong>Arthur GUILLERMIN HAZAN</strong>
+                <img src="img/thedude.png"></p>
+        </div>
+        <div class="ikonice">
+            <a class="fa fa-star" href="#"></a>
+            <a class="fa fa-heart" href="#"></a>
+            <a class="fa fa-instagram" href="#"></a>
+        </div>
+
+    </div>
+    </div>
+
+</header>
+
+<article>
+    <div class="container">
+        <div class="clanak">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis ex a pretium semper. Pellentesque at ante sem. Etiam efficitur erat nec feugiat auctor. Aliquam non risus sit amet ligula varius aliquam. Donec feugiat, leo et congue consectetur, nisi justo dignissim elit, vitae feugiat lectus urna vitae enim. Donec venenatis ipsum vitae ante consequat, in cursus massa auctor. Nullam posuere mi lectus, eget dapibus purus dictum ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <p>Curabitur viverra lorem at arcu egestas, quis vestibulum ante sollicitudin. Mauris ut bibendum nibh. Ut eu suscipit neque. Etiam mollis in dolor sed facilisis. Mauris mauris lacus, congue sed pharetra et, consectetur a lectus. Aliquam erat volutpat. Curabitur id pulvinar augue, et mollis nulla. Nunc in dapibus nunc, rhoncus consequat turpis. Proin pulvinar elementum placerat. Nullam ac tellus et enim ullamcorper molestie. Suspendisse at feugiat eros. Vestibulum fringilla justo nec eros commodo, id euismod risus condimentum. Sed vel pellentesque arcu. Maecenas ultrices neque eget nunc congue feugiat. Praesent gravida quis lectus eget pharetra.</p>
+            <q>This is a long,neat,and pointless quote</q>
+            <p>Vivamus vestibulum dignissim dignissim. Pellentesque sollicitudin, quam sit amet elementum facilisis, ipsum sapien varius odio, ut dictum lorem diam vel augue. Nam ut felis purus. Curabitur malesuada malesuada libero. Maecenas lobortis pharetra odio, auctor rhoncus erat condimentum non. Vestibulum vitae justo ut mi sollicitudin luctus. Praesent sit amet tincidunt sem, sed pulvinar sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec tristique eget ipsum a bibendum. Donec sit amet mauris augue. Curabitur augue quam, scelerisque in ante sit amet, rutrum porta velit. Nunc in dignissim ex. Sed in tempor erat, ut placerat dolor. Curabitur dictum, velit non ultricies egestas, tortor turpis sodales purus, vitae consectetur elit odio a quam. Fusce tempus vitae nibh sed efficitur. Nunc eget condimentum turpis: </p>
+            <ul>
+                <li><p>Nulla facilisi. Aliquam sollicitudin massa eu arcu iaculis tincidunt. Mauris eu enim eget mauris cursus pretium eget non orci. Vestibulum placerat id diam a placerat. Nullam urna risus, pulvinar at aliquet a, elementum eu nulla. Duis at luctus eros. In sed facilisis elit. </p></li>
+                <li><p>Nulla facilisi. Aliquam sollicitudin massa eu arcu iaculis tincidunt. Mauris eu enim eget mauris cursus pretium eget non orci. Vestibulum placerat id diam a placerat. Nullam urna risus, pulvinar at aliquet a, elementum eu nulla. Duis at luctus eros. In sed facilisis elit. </p></li>
+                <li><p>Nulla facilisi. Aliquam sollicitudin massa eu arcu iaculis tincidunt. Mauris eu enim eget mauris cursus pretium eget non orci. Vestibulum placerat id diam a placerat. Nullam urna risus, pulvinar at aliquet a, elementum eu nulla. Duis at luctus eros. In sed facilisis elit. </p></li>
+            </ul>
+        </div>
+        <div class="btn1"><a class="fa fa-hand-pointer-o" href="#">
+                <br>BACK TO TOP</a>
+        </div>
+    </div>
+</article>
+
+<footer>
+    <div class="btn2"><a class="fa fa-share-square-o" href="#"> SHARE THIS ARTICLE</a>
+    </div>
+    <div class="futercelina">
+
+        <div class="futertext1">
+            <h4>NEXT IN YOUR READING LIST...</h4>
+            <h2>Seaside, no thanks...</h2>
+            <h3>This is an awesome headline</h3>
+        </div>
+
+
+
+
+        <div class="futertext2">
+            <h4>NEXT IN YOUR READING LIST...</h4>
+            <h2>Roma,bella vita...</h2>
+            <h3>This is an awesome headline</h3>
+        </div>
+
+    </div>
+</footer>
+</body>
+</html>
+
